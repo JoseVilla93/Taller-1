cc3193679a9feb691c873bb1f81aaabb56737739
Jose Villa <jcabal217@soyudemedellin.edu.co>
2025-09-04 00:27:42 +0000

    puntos

---
+++ 3.main.py
@@ -0,0 +1,6 @@
+alcancia = [100, 500, 200]  # Billetes o monedas
+
+print("Contenido de la alcancia:", alcancia)
+
+total = sum(alcancia)
+print(f"El total de dinero en la alcancia es: {total} pesos")
